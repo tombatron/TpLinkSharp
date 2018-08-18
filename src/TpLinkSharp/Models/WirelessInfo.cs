b68ea389f6a058e909d07fc37c66f47934dda38d
Tom Hanks <tommy.hanks@gmail.com>
2018-08-18 16:09:14 -0400

    First pass at implementing the wireless info class.

--- src/TpLinkSharp/Models/WirelessInfo.cs
+++ src/TpLinkSharp/Models/WirelessInfo.cs
@@ -1,13 +1,148 @@
+using static TpLinkSharp.Utilities.StringUtilities;
+
 namespace TpLinkSharp.Models
 {
     public class WirelessInfo
     {
-        public string WirelessRadio { get; }
-        public string NameSsid { get; }
-        public string Mode { get; }
-        public string Channel { get; }
-        public string ChannelWidth { get; }
-        public string MacAddress { get; }
-        public string WdsStatus { get; }
+        private readonly static string[] _wlanTypes = new[]
+        {
+            "11b only",
+            "11g only",
+            "11n only",
+            "11bg mixed",
+            "11bgn mixed",
+            "11a only",
+            "11n only",
+            "11an mixed",
+            "11ac only",
+            "11a/n/ac mixed",
+        };
+
+        private readonly static string[] _channelWidths = new[]
+        {
+            "20MHz",
+            "Automatic",
+            "40MHz"
+        };
+
+        private const int WirelessRadioStatusIndex = 0;
+        private const int NameSsidIndex = 1;
+        private const int ChannelIndex = 2;
+        private const int ModeIndex = 3;
+        private const int MacAddressIndex = 4;
+        private const int IpAddressIndex = 5;
+        private const int ChannelWidthIndex = 6;
+        private const int AutoChannelIndex = 9;
+        private const int WdsStatusIndex = 10;
+
+        private string _wirelessRadio;
+        private string _nameSsid;
+        private string _mode;
+        private string _channel;
+        private string _channelWidth;
+        private string _macAddress;
+        private string _wdsStatus;
+
+        public string WirelessRadio
+        {
+            get => _wirelessRadio;
+
+            private set => _wirelessRadio = value == "1" ? "Enabled" : "Disabled";
+        }
+
+        public string NameSsid
+        {
+            get => _nameSsid;
+
+            private set => _nameSsid = RemoveLeadingAndTrailingQuotes(value);
+        }
+
+        public string Mode
+        {
+            get => _mode;
+
+            private set => _mode = _wlanTypes[int.Parse(value)];
+        }
+
+        public string Channel
+        {
+            get => _channel;
+
+            set => _channel = value.Trim();
+        }
+
+        public string ChannelWidth
+        {
+            get => _channelWidth;
+
+            set => _channelWidth = _channelWidths[int.Parse(value)];
+        }
+
+        public string MacAddress
+        {
+            get => _macAddress;
+
+            set => _macAddress = RemoveLeadingAndTrailingQuotes(value);
+        }
+
+        public string WdsStatus
+        {
+            get => _wdsStatus;
+
+            set => GetWdsStatus(value);
+        }
+
+        public WirelessInfo(string[] wirelessInfoArray)
+        {
+            WirelessRadio = wirelessInfoArray[WirelessRadioStatusIndex];
+            NameSsid = wirelessInfoArray[NameSsidIndex];
+            Mode = wirelessInfoArray[ModeIndex];
+            Channel = wirelessInfoArray[ChannelIndex];
+            ChannelWidth = wirelessInfoArray[ChannelWidthIndex];
+            MacAddress = wirelessInfoArray[MacAddressIndex];
+            WdsStatus = wirelessInfoArray[WdsStatusIndex];
+        }
+
+        private static string GetWdsStatus(string wdsStatusId)
+        {
+            string result;
+
+            switch (wdsStatusId)
+            {
+                case "0":
+                    result = "Init...";
+                    break;
+
+                case "1":
+                    result = "Scan...";
+                    break;
+
+                case "2":
+                    result = "Join...";
+                    break;
+
+                case "3":
+                    result = "Auth...";
+                    break;
+
+                case "4":
+                    result = "Assoc...";
+                    break;
+
+                case "5":
+                    result = "Run";
+                    break;
+
+                case "6":
+                    result = "Disable";
+                    break;
+
+                default:
+                    result = "Invalid";
+                    break;
+            }
+
+            return result;
+        }
     }
 }
