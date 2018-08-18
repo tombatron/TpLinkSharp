5fb3e6b194fe62b784fd9d695cbb8357fed37e51
Tom Hanks <tommy.hanks@gmail.com>
2018-08-18 18:21:58 -0400

    Setup unit tests.

---
+++ test/TpLinkSharp.UnitTests/Models/WanInfoTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace TpLinkSharp.UnitTests.Models
+{
+    public class WanInfoTests
+    {
+        public class ItCanInitialize
+        {
+            private static readonly string[] WanInfoArray = new[] { "4", "\"F4-F2-6D-94-C8-FE\"", "\"99.138.9.19\"", "1", "\"255.255.88.0\"", "0", "0", "\"77.138.0.1\"", "1", "1", "0", "\"75.58.81.1 , 11.18.47.61\"", "\"\"", "0", "0", "\"0.0.0.0\"", "\"0.0.0.0\"", "\"0.0.0.0\"", "\"0.0.0.0 , 0.0.0.0\"", "0", "0", "0", "0", "0", "0", "0" };
+
+            [Fact]
+            public void MacAddressFromWanInfoArray()
+            {
+
+            }
+
+            [Fact]
+            public void IpAddressFromWanInfoArray()
+            {
+
+            }
+
+            [Fact]
+            public void SubnetMaskFromWanInfoArray()
+            {
+
+            }
+
+            [Fact]
+            public void DnsServersFromWanInfoArray()
+            {
+
+            }
+        }
+    }
+}
