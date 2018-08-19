c03b4f25567081cbeaf41fb4f4dcbc5967968aa8
Tom Hanks <tommy.hanks@gmail.com>
2018-08-19 17:25:14 -0400

    Implemented a test project to emit the current bandwidth usage.

--- test/TpLinkSharp.TestConsole/Program.cs
+++ test/TpLinkSharp.TestConsole/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using TpLinkSharp;
+using System.Threading;
 
 namespace TpLinkSharp.TestConsole
 {
@@ -7,19 +7,49 @@ namespace TpLinkSharp.TestConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press `ESC` to exit...");
+
             var username = Environment.GetEnvironmentVariable("ROUTER_USERNAME");
             var password = Environment.GetEnvironmentVariable("ROUTER_PASSWORD");
 
-            using(var client = new TpLinkClient("http://192.168.0.1"))
+            var previousReceivedBytes = default(long);
+            var previousSentBytes = default(long);
+
+            using (var client = new TpLinkClient("http://192.168.0.1"))
             {
                 client.Connect(username, password);
 
                 var commands = new TpLinkCommands(client);
 
-                var status = commands.GetCurrentStatus().GetAwaiter().GetResult();
-            }
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        var status = commands.GetCurrentStatus().GetAwaiter().GetResult();
 
-            Console.ReadLine();
+                        if (previousReceivedBytes == default(int) && previousSentBytes == default(int))
+                        {
+                            previousReceivedBytes = status.TrafficStatistics.Received.Bytes;
+                            previousSentBytes = status.TrafficStatistics.Sent.Bytes;
+                        }
+                        else
+                        {
+                            var currentReceivedBytes = status.TrafficStatistics.Received.Bytes;
+                            var currentSentBytes = status.TrafficStatistics.Sent.Bytes;
+
+                            var receiveRate = (((currentReceivedBytes - previousReceivedBytes) / 2) * 8) / 1000000f;
+                            var sendRate = (((currentSentBytes - previousSentBytes) / 2) * 8) / 1000000f;
+
+                            Console.WriteLine($"\rReceive Rate: {receiveRate.ToString("0.000")}Mbps | Sent Rate: {sendRate.ToString("0.000")}Mbps");
+
+                            previousReceivedBytes = currentReceivedBytes;
+                            previousSentBytes = currentSentBytes;
+                        }
+
+                        Thread.Sleep(2000);
+                    }
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
         }
     }
 }
