f1f4074341034e3e2f8c8fedf01fed77f76931ee
Tom Hanks <tommy.hanks@gmail.com>
2018-08-18 21:07:28 -0400

    Implemented waninfo.

--- src/TpLinkSharp/Models/WanInfo.cs
+++ src/TpLinkSharp/Models/WanInfo.cs
@@ -1,11 +1,61 @@
+using System.Linq;
+using static TpLinkSharp.Utilities.StringUtilities;
+
 namespace TpLinkSharp.Models
 {
     public class WanInfo
     {
-        public string MacAddress { get; }
-        public string IpAddress { get; }
-        public string SubnetMask { get; }
-        public string DefaultGateway { get; }
+        private const int MacAddressIndex = 1;
+        private const int IpAddressIndex = 2;
+        private const int SubnetMaskIndex = 4;
+        private const int DefaultGatewayIndex = 7;
+        private const int DnsServerIndex = 11;
+
+        private string _macAddress;
+        private string _ipAddress;
+        private string _subnetMask;
+        private string _defaultGateway;
+
+        public string MacAddress
+        {
+            get => _macAddress;
+
+            private set => _macAddress = RemoveLeadingAndTrailingQuotes(value);
+        }
+
+        public string IpAddress
+        {
+            get => _ipAddress;
+
+            private set => _ipAddress = RemoveLeadingAndTrailingQuotes(value);
+        }
+
+        public string SubnetMask
+        {
+            get => _subnetMask;
+
+            private set => _subnetMask = RemoveLeadingAndTrailingQuotes(value);
+        }
+
+        public string DefaultGateway
+        {
+            get => _defaultGateway;
+
+            private set => _defaultGateway = RemoveLeadingAndTrailingQuotes(value);
+        }
+
         public string[] DnsServer { get; }
+
+        public WanInfo(string[] wanInfoArray)
+        {
+            MacAddress = wanInfoArray[MacAddressIndex];
+            IpAddress = wanInfoArray[IpAddressIndex];
+            SubnetMask = wanInfoArray[SubnetMaskIndex];
+            DefaultGateway = wanInfoArray[DefaultGatewayIndex];
+            DnsServer = ParseDnsServer(wanInfoArray[DnsServerIndex]);
+        }
+
+        private static string[] ParseDnsServer(string dnsServers) =>
+            RemoveLeadingAndTrailingQuotes(dnsServers).Split(',').Select(x => x.Trim()).ToArray();
     }
 }
--- test/TpLinkSharp.UnitTests/Models/WanInfoTests.cs
+++ test/TpLinkSharp.UnitTests/Models/WanInfoTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using TpLinkSharp.Models;
+using Xunit;
 
 namespace TpLinkSharp.UnitTests.Models
 {
@@ -7,29 +8,36 @@ namespace TpLinkSharp.UnitTests.Models
         public class ItCanInitialize
         {
             private static readonly string[] WanInfoArray = new[] { "4", "\"F4-F2-6D-94-C8-FE\"", "\"99.138.9.19\"", "1", "\"255.255.88.0\"", "0", "0", "\"77.138.0.1\"", "1", "1", "0", "\"75.58.81.1 , 11.18.47.61\"", "\"\"", "0", "0", "\"0.0.0.0\"", "\"0.0.0.0\"", "\"0.0.0.0\"", "\"0.0.0.0 , 0.0.0.0\"", "0", "0", "0", "0", "0", "0", "0" };
+            private static readonly WanInfo WanInfo = new WanInfo(WanInfoArray);
 
             [Fact]
             public void MacAddressFromWanInfoArray()
             {
-
+                Assert.Equal("F4-F2-6D-94-C8-FE", WanInfo.MacAddress);
             }
 
             [Fact]
             public void IpAddressFromWanInfoArray()
             {
-
+                Assert.Equal("99.138.9.19", WanInfo.IpAddress);
             }
 
             [Fact]
             public void SubnetMaskFromWanInfoArray()
             {
+                Assert.Equal("255.255.88.0", WanInfo.SubnetMask);
+            }
 
+            [Fact]
+            public void DefaultGatewayFromWanInfoArray()
+            {
+                Assert.Equal("77.138.0.1", WanInfo.DefaultGateway);
             }
 
             [Fact]
             public void DnsServersFromWanInfoArray()
             {
-
+                Assert.Equal(new[] { "75.58.81.1", "11.18.47.61" }, WanInfo.DnsServer);
             }
         }
     }
