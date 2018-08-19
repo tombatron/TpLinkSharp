768d91a06dd8249127f7c060c763298c1de6d7f4
Tom Hanks <tommy.hanks@gmail.com>
2018-08-18 21:19:43 -0400

    Implemented traffic statistics.

--- src/TpLinkSharp/Models/BandwithUsageInfo.cs
+++ src/TpLinkSharp/Models/BandwithUsageInfo.cs
@@ -4,5 +4,16 @@ namespace TpLinkSharp.Models
     {
         public long Bytes { get; }
         public long Packets { get; }
+
+        private BandwithUsageInfo(long bytes, long packets)
+        {
+            Bytes = bytes;
+            Packets = packets;
+        }
+
+        public static BandwithUsageInfo Create(string bytes, string packets)
+        {
+            return new BandwithUsageInfo(long.Parse(bytes), long.Parse(packets));
+        }
     }
 }
--- src/TpLinkSharp/Models/TrafficStatisticsInfo.cs
+++ src/TpLinkSharp/Models/TrafficStatisticsInfo.cs
@@ -2,7 +2,18 @@ namespace TpLinkSharp.Models
 {
     public class TrafficStatisticsInfo
     {
+        private const int BytesReceivedIndex = 0;
+        private const int BytesSentIndex = 1;
+        private const int PacketsReceivedIndex = 2;
+        private const int PacketsSentIndex = 3;
+
         public BandwithUsageInfo Received { get; }
         public BandwithUsageInfo Sent { get; }
+
+        public TrafficStatisticsInfo(string[] trafficStatisitcsInfoArray)
+        {
+            Received = BandwithUsageInfo.Create(trafficStatisitcsInfoArray[BytesReceivedIndex], trafficStatisitcsInfoArray[PacketsReceivedIndex]);
+            Sent = BandwithUsageInfo.Create(trafficStatisitcsInfoArray[BytesSentIndex], trafficStatisitcsInfoArray[PacketsSentIndex]);
+        }
     }
 }
