9c300345f43542d271675c169f0a693b4f750438
Tom Hanks <tommy.hanks@gmail.com>
2018-08-19 17:34:03 -0400

    Fixed code formatting.

--- src/TpLinkSharp/TpLinkClient.cs
+++ src/TpLinkSharp/TpLinkClient.cs
@@ -11,39 +11,39 @@ namespace TpLinkSharp
 {
     public class TpLinkClient : IDisposable
     {
-        private static Regex ValidAuthenticationStringPattern = new Regex ("^[\x21-\x7e]+$", RegexOptions.Compiled);
-        private static Regex UrlPattern = new Regex (@"(http|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Compiled);
+        private static Regex ValidAuthenticationStringPattern = new Regex("^[\x21-\x7e]+$", RegexOptions.Compiled);
+        private static Regex UrlPattern = new Regex(@"(http|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Compiled);
 
         private readonly CookieContainer _cookieContainer;
         private readonly HttpClientHandler _clientHandler;
         private readonly HttpClient _client;
         private readonly Uri _baseAdminUrl;
 
-        private string _securityToken = default (string);
+        private string _securityToken = default(string);
 
-        public TpLinkClient (string baseAdminUrl) : this (new Uri (baseAdminUrl)) { }
+        public TpLinkClient(string baseAdminUrl) : this(new Uri(baseAdminUrl)) { }
 
-        public TpLinkClient (Uri baseAdminUrl)
+        public TpLinkClient(Uri baseAdminUrl)
         {
             _baseAdminUrl = baseAdminUrl;
 
-            _cookieContainer = new CookieContainer (capacity: 1);
+            _cookieContainer = new CookieContainer(capacity: 1);
             _clientHandler = new HttpClientHandler { CookieContainer = _cookieContainer, AllowAutoRedirect = true };
-            _client = new HttpClient (_clientHandler) { BaseAddress = _baseAdminUrl };
+            _client = new HttpClient(_clientHandler) { BaseAddress = _baseAdminUrl };
         }
 
-        public void Connect (string username, string password)
+        public void Connect(string username, string password)
         {
-            if (IsValidUsername (username) && IsValidPassword (password))
+            if (IsValidUsername(username) && IsValidPassword(password))
             {
-                var authCookie = CreateAuthenticationCookie (username, password);
+                var authCookie = CreateAuthenticationCookie(username, password);
 
-                _cookieContainer.Add (authCookie);
+                _cookieContainer.Add(authCookie);
 
-                var response = _client.GetAsync ("/userRpm/LoginRpm.htm?Save=Save").GetAwaiter ().GetResult ();
-                var responseContent = response.Content.ReadAsStringAsync ().GetAwaiter ().GetResult ();
+                var response = _client.GetAsync("/userRpm/LoginRpm.htm?Save=Save").GetAwaiter().GetResult();
+                var responseContent = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-                _securityToken = UrlPattern.Match (responseContent).Groups[3].Value.Split ('/') [1];
+                _securityToken = UrlPattern.Match(responseContent).Groups[3].Value.Split('/')[1];
             }
             else
             {
@@ -51,49 +51,49 @@ namespace TpLinkSharp
             }
         }
 
-        public void Disconnect ()
+        public void Disconnect()
         {
-            if (_securityToken != default (string))
+            if (_securityToken != default(string))
             {
-                var response = _client.GetAsync ("/userRpm/LogoutRpm.htm").GetAwaiter ().GetResult ();
+                var response = _client.GetAsync("/userRpm/LogoutRpm.htm").GetAwaiter().GetResult();
 
                 // TODO: If logout is unsucessful, throw an exception.
 
-                _securityToken = default (string);
+                _securityToken = default(string);
             }
         }
 
-        internal async Task<string> SendSecuredCommand (string path)
+        internal async Task<string> SendSecuredCommand(string path)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"/{_securityToken}{path}");
             requestMessage.Headers.Add("Referer", $"{_baseAdminUrl}/{_securityToken}/userRpm/MenuRpm.htm");
 
             var response = await _client.SendAsync(requestMessage);
 
-            return await response.Content.ReadAsStringAsync ();
+            return await response.Content.ReadAsStringAsync();
         }
 
-        public void Dispose ()
+        public void Dispose()
         {
-            Disconnect ();
+            Disconnect();
 
-            _client.Dispose ();
-            _clientHandler.Dispose ();
+            _client.Dispose();
+            _clientHandler.Dispose();
         }
 
-        private static Cookie CreateAuthenticationCookie (string username, string password) =>
-            new Cookie ("Authorization", $"{CreateAuthenticationString(username, password)}", "/", "192.168.0.1");
+        private static Cookie CreateAuthenticationCookie(string username, string password) =>
+            new Cookie("Authorization", $"{CreateAuthenticationString(username, password)}", "/", "192.168.0.1");
 
-        private static string CreateAuthenticationString (string username, string password) =>
-            Escape ($"Basic {Base64Encode($"{username}:{HashString (password.Substring (0, 15))}")}");
+        private static string CreateAuthenticationString(string username, string password) =>
+            Escape($"Basic {Base64Encode($"{username}:{HashString(password.Substring(0, 15))}")}");
 
-        private static bool IsAuthenticationSuccessful (string responseBody) => responseBody.Contains ("httpAutErrorArray");
+        private static bool IsAuthenticationSuccessful(string responseBody) => responseBody.Contains("httpAutErrorArray");
 
-        private static bool IsValidUsername (string username) => HasValidAuthenticationCharacters (username);
+        private static bool IsValidUsername(string username) => HasValidAuthenticationCharacters(username);
 
-        private static bool IsValidPassword (string password) => HasValidAuthenticationCharacters (password);
+        private static bool IsValidPassword(string password) => HasValidAuthenticationCharacters(password);
 
-        private static bool HasValidAuthenticationCharacters (string authenticationString) =>
-            authenticationString.Length > 0 && ValidAuthenticationStringPattern.IsMatch (authenticationString);
+        private static bool HasValidAuthenticationCharacters(string authenticationString) =>
+            authenticationString.Length > 0 && ValidAuthenticationStringPattern.IsMatch(authenticationString);
     }
 }
