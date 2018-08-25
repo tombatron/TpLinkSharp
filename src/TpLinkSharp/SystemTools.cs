f372f7423f4a0c3c69242e6c13eb396ba47608e4
Tom Hanks <tommy.hanks@gmail.com>
2018-08-25 08:43:06 -0400

    Added constructor that accepts an instance of the TpLinkClient class.

--- src/TpLinkSharp/SystemTools.cs
+++ src/TpLinkSharp/SystemTools.cs
@@ -6,6 +6,13 @@ namespace TpLinkSharp
 {
     public class SystemTools : ISystemTools
     {
+        private readonly TpLinkClient _client;
+
+        public SystemTools(TpLinkClient client)
+        {
+            _client = client;
+        }
+
         public Task<IEnumerable<BandwidthStatistics>> GetStatisticsAsync()
         {
             throw new System.NotImplementedException();
