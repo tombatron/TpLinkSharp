0c18d0d8fae87e02f349017a758dc677c780d428
Tom Hanks <tommy.hanks@gmail.com>
2018-08-25 11:33:10 -0400

    Added covering unit test for the new string utility.

--- test/TpLinkSharp.UnitTests/Utilities/StringUtilitiesTests.cs
+++ test/TpLinkSharp.UnitTests/Utilities/StringUtilitiesTests.cs
@@ -3,7 +3,7 @@ using static TpLinkSharp.Utilities.StringUtilities;
 
 namespace TpLinkSharp.UnitTests.Utilities
 {
-    public class StringUtilitiesTests
+    public class StringUtilitiesTests : BaseTest
     {
         [Fact]
         public void ItCanBase64EncodeAString()
@@ -23,6 +23,17 @@ namespace TpLinkSharp.UnitTests.Utilities
             var stringWithRemovedQuotes = RemoveLeadingAndTrailingQuotes(testString);
 
             Assert.Equal("hello world", stringWithRemovedQuotes);
+        }
+
+        [Fact]
+        public void ItCanExtractJavascriptArrays()
+        {
+            var testContent = ReadTestAssetContent("StatusRpmResponse.html");
+
+            var extractedArrays = ExtractJavascriptArrays(testContent);
+
+            Assert.Equal(23, extractedArrays.Length);
+            Assert.Equal(11, extractedArrays[0].Length);
         }
     }
 }
