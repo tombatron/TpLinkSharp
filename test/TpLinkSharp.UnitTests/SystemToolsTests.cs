5e8dfc253770472c8c9ab274177f817bad8aaa5b
Tom Hanks <tommy.hanks@gmail.com>
2018-09-02 08:49:41 -0400

    Added covering unit test for the SystemTools commands.

---
+++ test/TpLinkSharp.UnitTests/SystemToolsTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TpLinkSharp.UnitTests
+{
+    public class SystemToolsTests : BaseTest
+    {
+        [Fact]
+        public async Task ItCanGetStatistics()
+        {
+            var mockCommandRunner = new Mock<ITpLinkCommandRunner>();
+
+            mockCommandRunner
+                .Setup(x => x.SendSecuredCommand(It.Is<string>(s => s.StartsWith("/userRpm/SystemStatisticRpm.htm"))))
+                .ReturnsAsync(ReadTestAssetContent("SystemStatisticRpm.html"));
+
+            var systemToolsCommands = new SystemTools(mockCommandRunner.Object);
+
+            var result = await systemToolsCommands.GetStatisticsAsync();
+
+            Assert.True(result.Any());
+        }
+    }
+}
