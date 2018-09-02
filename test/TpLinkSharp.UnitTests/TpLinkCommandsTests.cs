d0f625ae2673e5196401c17cfbd84d16d98ca153
Tom Hanks <tommy.hanks@gmail.com>
2018-09-02 09:09:57 -0400

    Added some covering unit tests to the TpLinkCommands class.

---
+++ test/TpLinkSharp.UnitTests/TpLinkCommandsTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TpLinkSharp.UnitTests
+{
+    public class TpLinkCommandsTests : BaseTest
+    {
+        public class ItWillInitialize
+        {
+            [Fact]
+            public void TheSystemToolsCommands()
+            {
+                var mockCommandRunner = new Mock<ITpLinkCommandRunner>();
+
+                var tpLinkCommands = new TpLinkCommands(mockCommandRunner.Object);
+
+                Assert.NotNull(tpLinkCommands.SystemTools);
+            }
+        }
+
+        public class ItCanExecute
+        {
+            [Fact]
+            public async Task TheGetCurrentStatusCommand()
+            {
+                var mockCommandRunner = new Mock<ITpLinkCommandRunner>();
+
+                mockCommandRunner
+                    .Setup(x => x.SendSecuredCommand(It.Is<string>(s => s.StartsWith("/userRpm/StatusRpm.htm"))))
+                    .ReturnsAsync(ReadTestAssetContent("StatusRpmResponse.html"));
+
+                var tpLinkCommands = new TpLinkCommands(mockCommandRunner.Object);
+
+                var result = await tpLinkCommands.GetCurrentStatus();
+
+                Assert.NotNull(result);
+            }
+        }
+    }
+}
